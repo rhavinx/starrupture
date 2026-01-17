65e3ec3e55ba433c06e541097ac3bb5e30627c52
Teejo75 <437200+teejo75@users.noreply.github.com>
2026-01-17 14:52:18 +1000

    Moved functionality in to start.sh

--- remove_server_files.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo -n "Removing Server files from ${SERVERHOME}: "
-if [ -d ${SERVERHOME}/StarRupture ]; then
-    rm -rf ${SERVERHOME}/*
-    echo "done."
-else echo "failed.\n This may not be the server directory. Please manually empty it."
-fi
-
